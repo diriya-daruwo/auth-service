6c86fddba72072ac8b742b85d42ad6f5accd0f47
eranga herath <erangaeb@gmail.com>
2015-10-03 21:13:18 +0530

    added registration functionality

--- auth_api/models.py
+++ auth_api/models.py
@@ -1,3 +1,5 @@
+import binascii
+import os
 from django.db import models
 
 
@@ -11,3 +13,14 @@ class User(models.Model):
     username = models.CharField(max_length=64, unique=True)
     password = models.CharField(max_length=100)
     auth_token = models.CharField(max_length=100)
+
+    def save(self, *args, **kwargs):
+        if not self.auth_token:
+            self.auth_token = self.generate_auth_token()
+        return super(User, self).save(*args, **kwargs)
+
+    def generate_auth_token(self):
+        return binascii.hexlify(os.urandom(20)).decode()
+
+    def __unicode__(self):
+        return self.auth_token
--- auth_api/views.py
+++ auth_api/views.py
@@ -1,1 +1,30 @@
-# Create your views here.
+from django.db import IntegrityError
+from django.views.decorators.csrf import csrf_exempt
+from auth_api.models import User
+from auth_api.utils import json_response
+
+
+@csrf_exempt
+def register(request):
+    if request.method == 'POST':
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+
+        if username is not None and password is not None:
+            try:
+                user = User.objects.create(username=username,
+                                                    password=password)
+                return json_response({
+                            'token': user.auth_token,
+                            'username': user.username
+                        })
+            except IntegrityError:
+                return json_response({'error': 'User Exists'}, status=400)
+        else:
+            return json_response({'error': 'Invalid Data'}, status=400)
+    elif request.method == 'OPTIONS':
+        return json_response({})
+    else:
+        return json_response({
+            'error': 'Method not allowed'
+        }, status=405)
